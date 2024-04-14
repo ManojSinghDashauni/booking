b13a016d5554de5fb6aba60891a228a4936b2257
ManojSinghDashauni <Dashauni007@gmail.com>
2024-04-14 21:33:48 +0530

    booking fontend

---
+++ data/trending.ts
@@ -0,0 +1,37 @@
+export const trending_data = [
+    {
+        id:1,
+        src:"https://images.pexels.com/photos/208745/pexels-photo-208745.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"USA",
+        location:"United States Of America",
+        description:"15 Deals",
+    },
+    {
+        id:2,
+        src:"https://images.pexels.com/photos/210474/pexels-photo-210474.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"England",
+        location:"United Kingdom",
+        description:"25 Deals",
+    },
+    {
+        id:3,
+        src:"https://images.pexels.com/photos/10549876/pexels-photo-10549876.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"Dubai",
+        location:"United Arab Emirates",
+        description:"35 Deals",
+    },
+    {
+        id:4,
+        src:"https://images.pexels.com/photos/4612433/pexels-photo-4612433.png?auto=compress&cs=tinysrgb&w=600",
+        title:"Dubai",
+        location:"United Arab Emirates",
+        description:"45 Deals",
+    },
+    {
+        id:5,
+        src:"https://images.pexels.com/photos/776469/pexels-photo-776469.jpeg?auto=compress&cs=tinysrgb&w=600",
+        title:"France",
+        location:"France",
+        description:"50 Deals",
+    },
+]
